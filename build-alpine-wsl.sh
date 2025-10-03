0765f7ac1e5f34d7f127ea53ebe5a8588b375659
Flavian Diethelm <flavian.diethelm@eps.ch>
2025-10-03 11:59:17 +0200

    Update build-alpine-wsl.sh

--- build-alpine-wsl.sh
+++ build-alpine-wsl.sh
@@ -1,15 +1,18 @@
 #/bin/sh
 
-wget -O assets https://github.com/FlavianDiethelmEPS/Alpine-WSL/tree/main/assets
-chown root:root assets
-wget -O apline.tar.gz https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/alpine-minirootfs-3.22.0-x86_64.tar.gz
-mkdir rootfs
-cd rootfs
-tar -xzf ../apline.tar.gz
-mv ../assets .
+mkdir alpine
+cd alpine
+wget https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/alpine-minirootfs-3.22.0-x86_64.tar.gz -O - | tar -xz
+cd etc
+wget https://raw.githubusercontent.com/FlavianDiethelmEPS/Alpine-WSL/refs/heads/main/assets/etc/wsl-distribution.conf -O wsl-distribution.conf
+wget https://raw.githubusercontent.com/FlavianDiethelmEPS/Alpine-WSL/refs/heads/main/assets/etc/wsl.conf -O wsl.conf
+mkdir wsl
+cd wsl
+wget https://raw.githubusercontent.com/FlavianDiethelmEPS/Alpine-WSL/refs/heads/main/assets/etc/wsl/system-setup.sh -O system-setup.sh
+wget https://raw.githubusercontent.com/FlavianDiethelmEPS/Alpine-WSL/refs/heads/main/assets/etc/wsl/system-start.sh -O system-start.sh
+chmod u+x *
+cd ../..
 tar -c * | gzip --best > ../Alpine.wsl
 cd ..
-rm -rf rootfs
-rm -rf assets
-rm -rf apline.tar.gz
+rm -rf alpine
 echo "Done! Alpine.wsl created"
