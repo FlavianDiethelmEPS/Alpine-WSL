14d9fe393addf9528e281138dd8ec89fc30b2dc5
Flavian Diethelm <flavian.diethelm@eps.ch>
2025-10-03 12:48:23 +0200

    Update system-setup.sh

--- assets/etc/wsl/system-setup.sh
+++ assets/etc/wsl/system-setup.sh
@@ -3,5 +3,6 @@
 apk update
 apk upgrade
 apk add doas podman podman-compose iptables git openssh-client
+mkdir -p /etc/doas.d
 echo 'permit nopass :wheel' > /etc/doas.d/20-wheel.conf
 echo 'root:root' | chpasswd
